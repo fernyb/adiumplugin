5bad9bd7fd2180920167c48e1d332363aa3f0303
Fernando Barajas <fernyb@fernyb.net>
2009-04-19 08:31:07 -0700

    XCode Project Template to create Adium Plugins

---
+++ ___PROJECTNAMEASIDENTIFIER___.h
@@ -0,0 +1,18 @@
+//
+//  ___PROJECTNAMEASIDENTIFIER___.h
+//  ___PROJECTNAMEASIDENTIFIER___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  Copyright ___ORGANIZATIONNAME___ ___YEAR___. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <Adium/AIPlugin.h>
+#import <Adium/AISharedAdium.h>
+
+
+@interface ___PROJECTNAMEASIDENTIFIER___ : AIPlugin {
+
+}
+
+@end
---
+++ ___PROJECTNAMEASIDENTIFIER___.m
@@ -0,0 +1,24 @@
+//
+//  ___PROJECTNAMEASIDENTIFIER___.m
+//  ___PROJECTNAMEASIDENTIFIER___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  Copyright ___ORGANIZATIONNAME___ ___YEAR___. All rights reserved.
+//
+
+#import "___PROJECTNAMEASIDENTIFIER___.h"
+
+
+@implementation ___PROJECTNAMEASIDENTIFIER___
+
+- (void)installPlugin
+{
+  // Do something for installation
+}
+
+- (void)uninstallPlugin
+{
+  // Do something for uninstallion
+}
+
+@end
